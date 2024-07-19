c5f5327611bb7a45f6c7d524c09f10fc38074daa
MariuszBlajet <mariuszblajet@gmail.com>
2024-07-19 09:39:47 +0200

    feat: add /update endpoint and interface

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import { Data } from "./index";
 
 const prisma = new PrismaClient();
 const app = express();
@@ -7,13 +8,13 @@ const app = express();
 app.use(express.json());
 
 // fetch all facts
-app.get("/api/posts", async (req, res) => {
+app.get("/api/facts", async (req, res) => {
   const facts = await prisma.fact.findMany();
   res.json(facts);
 });
 
 // fetch fact
-app.get(`/api/post/:id`, async (req, res) => {
+app.get(`/api/fact/:id`, async (req, res) => {
   const { id } = req.params;
   const fact = await prisma.fact.findUnique({
     where: { id: Number(id) },
@@ -22,7 +23,7 @@ app.get(`/api/post/:id`, async (req, res) => {
 });
 
 // add fact
-app.post(`/api/post`, async (req, res) => {
+app.post(`/api/fact`, async (req, res) => {
   const { text, source, category } = req.body;
   const new_fact = await prisma.fact.create({
     data: {
@@ -34,10 +35,33 @@ app.post(`/api/post`, async (req, res) => {
   res.json(new_fact);
 });
 
-// update post
+// update fact
+app.put("/api/fact/update/:id", async (req, res) => {
+  const { id } = req.params;
+  const { text, source, category } = req.body;
 
-// delete post
-app.delete(`/api/post/:id`, async (req, res) => {
+  const data: Data = {};
+
+  if (text !== undefined) {
+    data.text = text;
+  }
+  if (source !== undefined) {
+    data.source = source;
+  }
+  if (category !== undefined) {
+    data.category = category;
+  }
+
+  const fact_to_update = await prisma.fact.update({
+    where: { id: Number(id) },
+    data,
+  });
+
+  res.json(fact_to_update);
+});
+
+// delete fact
+app.delete(`/api/fact/:id`, async (req, res) => {
   const { id } = req.params;
   const fact_to_delete = await prisma.fact.delete({
     where: { id: Number(id) },
---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+export interface Data {
+  text?: string;
+  source?: string;
+  category?: string;
+}
